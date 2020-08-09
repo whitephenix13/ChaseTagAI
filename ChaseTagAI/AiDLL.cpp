c80f04213a989e428e9f5415605349bbd680d5f7
whitephenix13 <alexandrelasbleis@yahoo.fr>
2020-08-09 18:34:57 +0200

    Used correct type to allow c#/c++ conversion. Created basic brain

--- ChaseTagAI/AiDLL.cpp
+++ ChaseTagAI/AiDLL.cpp
@@ -1,9 +1,13 @@
 #include "pch.h"
 #include "AiDLL.h"
 
+
 //extern "C" is necessary to export the reference from our functions to the DLL
 extern "C" {
-	bool Negate(bool val) {
-		return !val;
+	void MouseAIAction(CELL_TYPE* board, int xBoardSize, int zBoardSize, float xMousePos, float zMousePos, float xCatPos, float zCatPos, int actionsSize, float* outActions ) {
+		outActions[0] = xCatPos <0 ? 1:-1;
+		outActions[1] = zCatPos < 0 ? 1 : -1;
+		//TODO: do not force movement against the wall 
 	}
 }
+
--- ChaseTagAI/AiDLL.h
+++ ChaseTagAI/AiDLL.h
@@ -1,6 +1,13 @@
 #pragma once
+#include <utility>
+#include <array>
 #define AIDLL_API __declspec(dllexport) 
 
+enum CELL_TYPE {
+	EMPTY, WALL
+};
+
 extern "C" {
-	AIDLL_API bool Negate(bool val);
+	AIDLL_API void MouseAIAction(CELL_TYPE* board,int xBoardSize,int zBoardSize, float xMousePos, float zMousePos,float xCatPos,float zCatPos, int actionsSize, float* outActions);
 }
+
